ef8fc84337db1d9fdd5dc93880bd43a17e3c7105
Michael Williams <michael.williams@enspiral.com>
2023-05-11 02:48:57 +1200

    do dah ddoooo

--- rust/json-canon/src/ser.rs
+++ rust/json-canon/src/ser.rs
@@ -1,6 +1,5 @@
 use core::mem::replace;
 use core::num::FpCategory;
-use encode_unicode::{IterExt, Utf16Char};
 use serde::Serialize;
 use serde_json::{
     from_str,
@@ -8,9 +7,7 @@ use serde_json::{
     Result, Serializer, Value,
 };
 
-use std::io::{self, Error, ErrorKind, Write};
-
-use crate::entry::Entry;
+use std::{io::{self, Error, ErrorKind, Write}, collections::VecDeque};
 
 /// Serialize the given value as a String of JSON.
 ///
@@ -70,52 +67,181 @@ where
 }
 
 #[derive(Clone, Debug)]
+struct ObjectEntry {
+    key_orig: Vec<u8>,
+    key_ser: Vec<u8>,
+    value_ser: Vec<u8>,
+    is_key_done: bool
+}
+
+impl ObjectEntry {
+    fn new() -> Self {
+        Self {
+            key_orig: Vec::new(),
+            key_ser: Vec::new(),
+            value_ser: Vec::new(),
+            is_key_done: false,
+        }
+    }
+
+    fn push_orig(&mut self, bytes: &[u8]) {
+        if !self.is_key_done {
+            self.key_orig.extend_from_slice(bytes);
+        }
+    }
+
+    fn push_ser(&mut self, bytes: &[u8]) {
+        if self.is_key_done {
+            self.value_ser.extend_from_slice(bytes);
+        } else {
+            self.key_ser.extend_from_slice(bytes);
+        }
+    }
+
+    fn get_writer(&mut self) -> Box<dyn Write> {
+        if self.is_key_done {
+            Box::new(self.value_ser)
+        } else {
+            Box::new(self.key_ser)
+        }
+    }
+
+    fn end_key(&mut self) {
+        self.is_key_done = true;
+    }
+
+    fn write_out<W>(&self, first: bool, writer: &mut W) -> io::Result<()>
+    where
+        W: Write + ?Sized,
+    {
+        if !first {
+            writer.write_all(",".as_bytes())?;
+        }
+
+        writer.write_all(self.key_ser.as_slice())?;
+        writer.write_all(":".as_bytes())?;
+        writer.write_all(self.value_ser.as_slice())?;
+
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug)]
+struct ObjectEntries {
+    entries: Vec<ObjectEntry>,
+}
+
+impl ObjectEntries {
+    fn new() -> Self {
+        Self { entries: Vec::new() }
+    }
+
+    fn current(&mut self) -> io::Result<&mut ObjectEntry> {
+        self.entries.last_mut().ok_or_else(|| {
+            Error::new(ErrorKind::InvalidData, "Object entry requested when entry is not active.")
+        })
+    }
+
+    fn start_key(&mut self) {
+        self.entries.push(ObjectEntry::new())
+    }
+
+    fn end_key(&mut self) -> io::Result<()> {
+        Ok(self.current()?.end_key())
+    }
+
+    fn push_orig(&mut self, bytes: &[u8]) -> io::Result<()> {
+        Ok(self.current()?.push_orig(bytes))
+    }
+
+    fn push_ser(&mut self, bytes: &[u8]) -> io::Result<()> {
+        Ok(self.current()?.push_ser(bytes))
+    }
+
+    fn write_out<W>(&self, writer: &mut W) -> io::Result<()>
+    where
+        W: Write + ?Sized,
+    {
+        CompactFormatter.begin_object(writer)?;
+
+        // ...
+
+        CompactFormatter.end_object(writer)?;
+
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug)]
+struct ObjectStack {
+    stack: VecDeque<ObjectEntries>,
+}
+
+impl ObjectStack {
+    fn new() -> Self {
+        Self { stack: VecDeque::new() }
+    }
+
+    fn current(&mut self) -> io::Result<&mut ObjectEntries> {
+        self.stack
+            .front_mut()
+            .ok_or_else(|| {
+                Error::new(ErrorKind::InvalidData, "Object requested when object is not active.")
+            })
+    }
+    
+    fn has_current(&mut self) -> bool {
+        !self.stack.is_empty()
+    }
+
+    fn start_object(&mut self) {
+        self.stack.push_front(ObjectEntries::new())
+    }
+
+    fn start_key(&mut self) -> io::Result<()> {
+        Ok(self.current()?.start_key())
+    }
+
+    fn end_key(&mut self) -> io::Result<()> {
+        Ok(self.current()?.start_key())
+    }
+
+    fn push_orig(&mut self, bytes: &[u8]) -> io::Result<()> {
+        Ok(self.current()?.push_orig(bytes)?)
+    }
+
+    fn push_ser(&mut self, bytes: &[u8]) -> io::Result<()> {
+        Ok(self.current()?.push_ser(bytes)?)
+    }
+
+
+    fn end_object<W>(&mut self, writer: &mut W) -> io::Result<()>
+    where
+        W: Write + ?Sized,
+    {
+        let mut object = self.stack.pop_front().ok_or_else(|| {
+            Error::new(ErrorKind::InvalidData, "Object requested when object is not active.")
+        })?;
+
+        let mut writer = if self.has_current() {
+            self.current()?.current()?.get_writer()
+        } else {
+            Box::new(writer)
+        };
+
+        object.write_out(&mut writer)
+    }
+}
+
+#[derive(Clone, Debug)]
 #[repr(transparent)]
-pub struct CanonicalFormatter(Vec<Entry>);
+pub struct CanonicalFormatter {
+    stack: ObjectStack,
+}
 
 impl CanonicalFormatter {
     pub const fn new() -> Self {
-        Self(Vec::new())
-    }
-
-    pub fn scope<'a, W>(&'a mut self, writer: &'a mut W) -> Box<dyn Write + 'a>
-    where
-        W: Write + ?Sized,
-    {
-        match self.0.last_mut() {
-            Some(entry) if entry.complete => Box::new(&mut entry.next_val),
-            Some(entry) => Box::new(&mut entry.next_key),
-            None => Box::new(writer),
-        }
-    }
-
-    pub fn entry_mut(&mut self) -> io::Result<&mut Entry> {
-        self.0
-            .last_mut()
-            .ok_or_else(|| Error::new(ErrorKind::Other, "oh no"))
-    }
-
-    fn write_float<W, F>(
-        &mut self,
-        writer: &mut W,
-        category: FpCategory,
-        value: F,
-    ) -> io::Result<()>
-    where
-        W: Write + ?Sized,
-        F: ryu_js::Float,
-    {
-        match category {
-            FpCategory::Nan => Err(Error::new(ErrorKind::InvalidInput, "NaN is not allowed.")),
-            FpCategory::Infinite => Err(Error::new(
-                ErrorKind::InvalidInput,
-                "Infinity is not allowed.",
-            )),
-            FpCategory::Zero => self.scope(writer).write_all(b"0"),
-            FpCategory::Normal | FpCategory::Subnormal => self
-                .scope(writer)
-                .write_all(ryu_js::Buffer::new().format_finite(value).as_bytes()),
-        }
+        Self { stack: ObjectStack::new() }
     }
 }
 
@@ -140,35 +266,34 @@ impl Formatter for CanonicalFormatter {
         }
     }
 
-    #[inline]
     fn write_char_escape<W>(&mut self, writer: &mut W, escape: CharEscape) -> io::Result<()>
     where
         W: Write + ?Sized,
     {
-        #[inline]
-        fn serialize(control: u8) -> [u8; 6] {
-            static HEX: [u8; 16] = *b"0123456789abcdef";
-            [
-                b'\\',
-                b'u',
-                b'0',
-                b'0',
-                HEX[(control >> 4) as usize],
-                HEX[(control & 0xF) as usize],
-            ]
+        static HEX: [u8; 16] = *b"0123456789abcdef";
+
+        let serialized_bytes = match escape {
+            CharEscape::Quote => b"\\\"".to_vec(),
+            CharEscape::ReverseSolidus => b"\\\\".to_vec(),
+            CharEscape::Solidus => b"\\/".to_vec(),
+            CharEscape::Backspace => b"\\b".to_vec(),
+            CharEscape::FormFeed => b"\\f".to_vec(),
+            CharEscape::LineFeed => b"\\n".to_vec(),
+            CharEscape::CarriageReturn => b"\\r".to_vec(),
+            CharEscape::Tab => b"\\t".to_vec(),
+            CharEscape::AsciiControl(control) => {
+                [
+                    b'\\',
+                    b'u',
+                    b'0',
+                    b'0',
+                    HEX[(control >> 4) as usize],
+                    HEX[(control & 0xF) as usize],
+                ].to_vec()
+            }
         }
 
-        match escape {
-            CharEscape::Quote => self.scope(writer).write_all(b"\\\""),
-            CharEscape::ReverseSolidus => self.scope(writer).write_all(b"\\\\"),
-            CharEscape::Solidus => self.scope(writer).write_all(b"\\/"),
-            CharEscape::Backspace => self.scope(writer).write_all(b"\\b"),
-            CharEscape::FormFeed => self.scope(writer).write_all(b"\\f"),
-            CharEscape::LineFeed => self.scope(writer).write_all(b"\\n"),
-            CharEscape::CarriageReturn => self.scope(writer).write_all(b"\\r"),
-            CharEscape::Tab => self.scope(writer).write_all(b"\\t"),
-            CharEscape::AsciiControl(control) => self.scope(writer).write_all(&serialize(control)),
-        }
+        if 
     }
 
     #[inline]
@@ -463,5 +588,25 @@ impl Formatter for CanonicalFormatter {
         entry.object.insert(key, val);
 
         Ok(())
+    }
+}
+
+fn write_float<W, F>(
+    writer: &mut W,
+    category: FpCategory,
+    value: F,
+) -> io::Result<()>
+where
+    W: Write + ?Sized,
+    F: ryu_js::Float,
+{
+    match category {
+        FpCategory::Nan => Err(Error::new(ErrorKind::InvalidInput, "NaN is not allowed.")),
+        FpCategory::Infinite => Err(Error::new(
+            ErrorKind::InvalidInput,
+            "Infinity is not allowed.",
+        )),
+        FpCategory::Zero => writer.write_all(b"0"),
+        FpCategory::Normal | FpCategory::Subnormal => writer.write_all(ryu_js::Buffer::new().format_finite(value).as_bytes()),
     }
 }
