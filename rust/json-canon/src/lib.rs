0f2d84ae555ce61d2ce7fc3212725a06a0a22161
Michael Williams <michael.williams@enspiral.com>
2023-05-11 15:17:05 +1200

    more docs

--- rust/json-canon/src/lib.rs
+++ rust/json-canon/src/lib.rs
@@ -1,8 +1,35 @@
-//! JSON Canonicalization Scheme (JCS)
+//! # `json-canon`
 //!
-//! ### References
+//! Serialize JSON into a canonical format.
 //!
-//! [RFC 8785](https://tools.ietf.org/html/rfc8785)
+//! Safe for generating a consistent cryptographic hash or signature across platforms.
+//!
+//! Follows [RFC8785: JSON Canonicalization Scheme (JCS)]
+//!
+//! [RFC8785: JSON Canonicalization Scheme (JCS)]: https://tools.ietf.org/html/rfc8785
+//!
+//! ## Example
+//!
+//! ```rust
+//! # use json_canon::to_string;
+//! # use serde_json::{json, Error};
+//! # fn main() -> Result<(), Error> {
+//! let data = json!({
+//!     "from_account": "543 232 625-3",
+//!     "to_account": "321 567 636-4",
+//!     "amount": 500,
+//!     "currency": "USD"
+//! });
+//!
+//! println!("{}", to_string(&data)?);
+//! // {"amount":500,"currency":"USD","from_account":"543 232 625-3","to_account":"321 567 636-4"}
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! ## Caveats
+//!
+//! `serde_json` deserializes `f64::NAN` and `f64::Infinite` as `None`, so if given a Rust struct with these values, the `json-canon` will currently output `"null"`.
 //!
 
 mod object;
