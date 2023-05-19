dc04621c0579cfe45b1153f1a2b34d2a5b37af43
Michael Williams <michael.williams@enspiral.com>
2023-05-19 21:53:41 +1200

    add more benches to play with

--- rust/json-canon/benches/basic.rs
+++ rust/json-canon/benches/basic.rs
@@ -2,12 +2,8 @@ use std::{env::current_dir, fs::read_to_string, path::Path};
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 
-use json_canon::to_string;
-use serde_json::{from_str, Value};
-
-fn bench_input(value: &Value) {
-    to_string(value).unwrap();
-}
+use json_canon::to_string as canon_to_string;
+use serde_json::{from_str, to_string as compact_to_string, Value};
 
 fn from_elem(c: &mut Criterion) {
     let test_data_path = current_dir()
@@ -17,9 +13,26 @@ fn from_elem(c: &mut Criterion) {
     let test_data: Value = from_str(&test_data_str).unwrap();
     let mut group = c.benchmark_group("from_elem");
     group.throughput(Throughput::Elements(1));
-    group.bench_with_input(BenchmarkId::new("basic", &test_data), &test_data, |b, v| {
-        b.iter(|| bench_input(&v));
+    group.bench_with_input(BenchmarkId::new("canon", &test_data), &test_data, |b, v| {
+        b.iter(|| canon_to_string(&v).unwrap());
     });
+    group.bench_with_input(
+        BenchmarkId::new("compact", &test_data),
+        &test_data,
+        |b, v| {
+            b.iter(|| compact_to_string(&v).unwrap());
+        },
+    );
+    group.bench_with_input(
+        BenchmarkId::new("ser_and_de", &test_data_str),
+        &test_data_str,
+        |b, s| {
+            b.iter(|| {
+                let v: Value = from_str(s).unwrap();
+                compact_to_string(&v).unwrap();
+            })
+        },
+    );
     group.finish();
 }
 
